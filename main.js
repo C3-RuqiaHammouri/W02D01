0123ad3bb45ff4bc3763050e40ae665b36eb3f8b
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-07-11 23:15:42 +0300

    Meaningful Message

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ console.log(arrowTitle)
 ////////////////////Q1//////////////////////
 
 // part 1
-const colors = ['black ','white ', 'green']
+const colors = ['black ', 'white ', 'green']
 //part 2 
-const negativeNumbers = [ -1 , -2 , -3 , -4 , -5]
+const negativeNumbers = [-1, -2, -3, -4, -5]
 //part 3 
-const food = [ " pizza ", 'mansaf ', ' yalanjee ']
+const food = [" pizza ", 'mansaf ', ' yalanjee ']
 //part 4 
-const numbers = [   [1 , 3 , 5 ]  ,    [2 , 4, 6 , 8 ]   ]
+const numbers = [[1, 3, 5], [2, 4, 6, 8]]
 
 ///////////////////Q2/////////////////////// 
 
@@ -23,45 +23,45 @@ const numbers = [   [1 , 3 , 5 ]  ,    [2 , 4, 6 , 8 ]   ]
 const orderedPlanets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter"];
 const unorderedPlanets = ["Mars", "Earth", "Mercury", "Jupiter", "Venus"];
 
- console.log (orderedPlanets [3])
- console.log (unorderedPlanets [0])
+console.log(orderedPlanets[3])
+console.log(unorderedPlanets[0])
 
 
- //part 2 
- //const animals = ["Cat", "Dog", "Dolphin", "Squirrel", "Koala"];
- //animals[animals.length-1]
+//part 2 
+//const animals = ["Cat", "Dog", "Dolphin", "Squirrel", "Koala"];
+//animals[animals.length-1]
 
 
- //////////////Q3 /////////////////////////
+//////////////Q3 /////////////////////////
 
 //part 1 
 
 //dont under stand it ;
 
 
- 
- //part 2
- const animals = ["Cat", "Dog", "Dolphin", "Squirrel"];
 
- animals.shift()
+//part 2
+const animals = ["Cat", "Dog", "Dolphin", "Squirrel"];
 
- animals.unshift('Koala ')
+animals.shift()
 
- 
- 
- 
- 
- 
- 
- 
- 
- //////////////////Q4 //////////////////////////
- const reptiles = ["Snake", "Lizard", "Turtle"];
+animals.unshift('Koala ')
+
+
+
+
+
+
+
+
+
+//////////////////Q4 //////////////////////////
+const reptiles = ["Snake", "Lizard", "Turtle"];
 //part 1   
- reptiles.push('Dinosaur')
+reptiles.push('Dinosaur')
 //part 2 
-const fish = ["Swordfish", "Clownfish", "Shark"];       
-fish.unshift( 'Goldfish')   
+const fish = ["Swordfish", "Clownfish", "Shark"];
+fish.unshift('Goldfish')
 // part 3
 reptiles.shift()
 // part 4 
@@ -75,74 +75,74 @@ fish.pop()
 const year_1 = [1, 9, 9, 3];
 console.log(year_1.join(""));
 
-  
+
 //part 2 
-const fullName = ["John ", "Doe  " ,"The Third "];
-console.log(fullName.join("")) ;
+const fullName = ["John ", "Doe  ", "The Third "];
+console.log(fullName.join(""));
 
 
 //part 3 
- const helloWorld =  ["hello", "world"];
- console.log(helloWorld.join(","))
+const helloWorld = ["hello", "world"];
+console.log(helloWorld.join(","))
 
 //part 4 
-const number_1= [1, 2, 3, 4];
+const number_1 = [1, 2, 3, 4];
 number_1.reverse
 console.log(number_1.reverse())
-     
-         
+
+
 
 /////////////////////practic ////////////////////
 //Q1
 
 const addToArray = function (array, string) {
-    return array= array+ string
-    
-  };
-  
+  return array = array + string
+
+};
+
 
 //Q2 
-const convertToString = function (array) {    
-   return (array.join("  "))
-  };
-  
-  
-  
+const convertToString = function (array) {
+  return (array.join("  "))
+};
+
+
+
 //Q3 
 
-  const accessElement = function (array, index) 
-  {
-    if (array.length > index )
-         {
-        return array[index]
-         }else 
-          {
-            return "No element at index 10"
-          }
-  };
-  
+const accessElement = function (array, index) {
+  if (array.length > index) {
+    return array[index]
+  } else {
+    return "No element at index 10"
+  }
+};
 
 
-  //Q4   
-  
-  const isInArray = function (array, string) 
-  { 
-      if (array.indexOf(string)) 
-      { return true
-      } 
-            else
-                  {
-                return false 
-                  }
+
+//Q4   
+
+const isInArray = function (array, string) {
+  if (array.indexOf(string)) {
+    return true
+  }
+  else {
+    return false
+  }
 };
 
 
 
 //Q5 
 
-
+//اول شي بعمل سبليت لكل كلمة بدخلها
+//  بعدين بفحص طول الاراي اللي صارت عن
+//عندي ازا طولها 1 معناها  هي كلمة وحدة ازا طولها اكتر من واحد 
+// معناها هي اكتر من كلكة وبطلق عليها الشروط  
+//
+//
 /*const reverseWords = function (string) 
-{  if (string.length == string.length ) 
+{  if () 
     {return string
 
     } if()
@@ -151,39 +151,39 @@ const convertToString = function (array) {
         
 
 };*/
- 
-  //Q6
 
-  const addToLast = function (array, value)
-   {
-      return value.unshift(array)
-   };
+//Q6
 
+const addToLast = function (array, value) {
+  return value.unshift(array)
+};
 
 
 
- //Q7
-   
+
+//Q7
+
 // part 1 
-const getLength = function (array)
- {   return (array.push())
-    
- };
-  
- 
-  
-  // write a function `getFirstVal` that accepts
-  // an array and returns the
-  // first value of the array without using the index to
-  // access the value
-  
-  
-  const getFirstVal = function (array)
-   {
-    
-   };
-  
-  getFirstVal([1, 2, 3, 4]); // => 1
-  getFirstVal([51, 3, 3, 4]); // => 51
-  
-  
+const getLength = function (array) {
+  return (array.push())
+
+};
+
+
+
+// PART 2 
+const getFirstVal = function (array) {
+  return array.shift();
+
+
+};
+
+
+
+
+
+//Q8 
+// the array values are unique, no duplicate values in the array
+const updateOrCreate = function (array, value, index) {
+  // TODO: Your code here
+};

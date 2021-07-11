0c37a56a43bde3e1ac307b0a90db57b0eb46d233
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-07-11 22:15:24 +0300

    Meaningful Message

---
+++ main.js
@@ -0,0 +1,189 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+
+
+
+
+///////////////////////////////Pulse Check///////////////////////////////////
+
+////////////////////Q1//////////////////////
+
+// part 1
+const colors = ['black ','white ', 'green']
+//part 2 
+const negativeNumbers = [ -1 , -2 , -3 , -4 , -5]
+//part 3 
+const food = [ " pizza ", 'mansaf ', ' yalanjee ']
+//part 4 
+const numbers = [   [1 , 3 , 5 ]  ,    [2 , 4, 6 , 8 ]   ]
+
+///////////////////Q2/////////////////////// 
+
+//part 1 
+const orderedPlanets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter"];
+const unorderedPlanets = ["Mars", "Earth", "Mercury", "Jupiter", "Venus"];
+
+ console.log (orderedPlanets [3])
+ console.log (unorderedPlanets [0])
+
+
+ //part 2 
+ //const animals = ["Cat", "Dog", "Dolphin", "Squirrel", "Koala"];
+ //animals[animals.length-1]
+
+
+ //////////////Q3 /////////////////////////
+
+//part 1 
+
+//dont under stand it ;
+
+
+ 
+ //part 2
+ const animals = ["Cat", "Dog", "Dolphin", "Squirrel"];
+
+ animals.shift()
+
+ animals.unshift('Koala ')
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ //////////////////Q4 //////////////////////////
+ const reptiles = ["Snake", "Lizard", "Turtle"];
+//part 1   
+ reptiles.push('Dinosaur')
+//part 2 
+const fish = ["Swordfish", "Clownfish", "Shark"];       
+fish.unshift( 'Goldfish')   
+// part 3
+reptiles.shift()
+// part 4 
+fish.pop()
+
+
+///////////////Q5///////////////////
+
+//part 1   
+
+const year_1 = [1, 9, 9, 3];
+console.log(year_1.join(""));
+
+  
+//part 2 
+const fullName = ["John ", "Doe  " ,"The Third "];
+console.log(fullName.join("")) ;
+
+
+//part 3 
+ const helloWorld =  ["hello", "world"];
+ console.log(helloWorld.join(","))
+
+//part 4 
+const number_1= [1, 2, 3, 4];
+number_1.reverse
+console.log(number_1.reverse())
+     
+         
+
+/////////////////////practic ////////////////////
+//Q1
+
+const addToArray = function (array, string) {
+    return array= array+ string
+    
+  };
+  
+
+//Q2 
+const convertToString = function (array) {    
+   return (array.join("  "))
+  };
+  
+  
+  
+//Q3 
+
+  const accessElement = function (array, index) 
+  {
+    if (array.length > index )
+         {
+        return array[index]
+         }else 
+          {
+            return "No element at index 10"
+          }
+  };
+  
+
+
+  //Q4   
+  
+  const isInArray = function (array, string) 
+  { 
+      if (array.indexOf(string)) 
+      { return true
+      } 
+            else
+                  {
+                return false 
+                  }
+};
+
+
+
+//Q5 
+
+
+/*const reverseWords = function (string) 
+{  if (string.length == string.length ) 
+    {return string
+
+    } if()
+        
+
+        
+
+};*/
+ 
+  //Q6
+
+  const addToLast = function (array, value)
+   {
+      return value.unshift(array)
+   };
+
+
+
+
+ //Q7
+   
+// part 1 
+const getLength = function (array)
+ {   return (array.push())
+    
+ };
+  
+ 
+  
+  // write a function `getFirstVal` that accepts
+  // an array and returns the
+  // first value of the array without using the index to
+  // access the value
+  
+  
+  const getFirstVal = function (array)
+   {
+    
+   };
+  
+  getFirstVal([1, 2, 3, 4]); // => 1
+  getFirstVal([51, 3, 3, 4]); // => 51
+  
+  
